843e16d61f90ce5bb6f9f319356e7916007c710b
rruss003 <38058930+rruss003@users.noreply.github.com>
2019-12-02 10:54:13 -0800

    Update shm.c

--- shm.c
+++ shm.c
@@ -47,7 +47,7 @@ if(index > -1){
   mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[index].frame), PTE_W|PTE_U);
   shm_table.shm_pages[index].refcnt++;
   *pointer=(char *)PGROUNDUP(p->sz);
-//   p->sz += PGSIZE;
+  p->sz += PGSIZE;
 }
 else{
   // Case 2
