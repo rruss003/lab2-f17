af3fa5653c26dee0be8d1db6c1c60f91d8a1f778
rruss003 <38058930+rruss003@users.noreply.github.com>
2019-12-02 10:50:14 -0800

    Update shm.c

--- shm.c
+++ shm.c
@@ -32,58 +32,58 @@ int shm_open(int id, char **pointer) {
 
 //you write this
 //Look through the shm_table to see if the id we are opening already exists
-int i = 0;
-int index = -1;
-acquire(&(shm_table.lock));
-for (i=0; i<64; i++){
-  if(shm_table.shm_pages[i].id == id){
-    index = i;
-    break;
-  }
-}
-struct proc* p = myproc();
-if(index > -1){
-  // Case 1
-  mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[index].frame), PTE_W|PTE_U);
-  shm_table.shm_pages[index].refcnt++;
-  *pointer=(char *)PGROUNDUP(p->sz);
-  p->sz += PGSIZE;
-}
-else{
-  // Case 2
-  for(i = 0; i<64; i++){
-    if(shm_table.shm_pages[i].id == 0){
-      shm_table.shm_pages[i].id = id;
-      shm_table.shm_pages[i].frame = kalloc();
-      shm_table.shm_pages[i].refcnt = 1;
-      memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
-      mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
-      *pointer=(char *)PGROUNDUP(p->sz);
-      p->sz += PGSIZE;
-      break;
-    }
-  }
-}
+// int i = 0;
+// int index = -1;
+// acquire(&(shm_table.lock));
+// for (i=0; i<64; i++){
+//   if(shm_table.shm_pages[i].id == id){
+//     index = i;
+//     break;
+//   }
+// }
+// struct proc* p = myproc();
+// if(index > -1){
+//   // Case 1
+//   mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[index].frame), PTE_W|PTE_U);
+//   shm_table.shm_pages[index].refcnt++;
+//   *pointer=(char *)PGROUNDUP(p->sz);
+//   p->sz += PGSIZE;
+// }
+// else{
+//   // Case 2
+//   for(i = 0; i<64; i++){
+//     if(shm_table.shm_pages[i].id == 0){
+//       shm_table.shm_pages[i].id = id;
+//       shm_table.shm_pages[i].frame = kalloc();
+//       shm_table.shm_pages[i].refcnt = 1;
+//       memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+//       mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+//       *pointer=(char *)PGROUNDUP(p->sz);
+//       p->sz += PGSIZE;
+//       break;
+//     }
+//   }
+// }
   
-release(&(shm_table.lock));
+// release(&(shm_table.lock));
 return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
-int i=0;
-acquire(&(shm_table.lock));
-for (i=0; i<64; i++){
-  if(shm_table.shm_pages[i].id == id){
-    shm_table.shm_pages[i].refcnt--;
-    if(shm_table.shm_pages[i].refcnt == 0){
-      shm_table.shm_pages[i].id =0;
-      shm_table.shm_pages[i].frame =0;
-    }
-    break;
-  }
-}
-release(&(shm_table.lock));
+// int i=0;
+// acquire(&(shm_table.lock));
+// for (i=0; i<64; i++){
+//   if(shm_table.shm_pages[i].id == id){
+//     shm_table.shm_pages[i].refcnt--;
+//     if(shm_table.shm_pages[i].refcnt == 0){
+//       shm_table.shm_pages[i].id =0;
+//       shm_table.shm_pages[i].frame =0;
+//     }
+//     break;
+//   }
+// }
+// release(&(shm_table.lock));
 
 return 0; //added to remove compiler warning -- you should decide what to return
 }
