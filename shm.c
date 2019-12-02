19cba8630f1a0e557e34917b72816f6e731faf72
rruss003 <38058930+rruss003@users.noreply.github.com>
2019-12-02 09:41:45 -0800

    Update shm.c

--- shm.c
+++ shm.c
@@ -64,6 +64,7 @@ else{
       shm_table.shm_pages[i].frame = kalloc();
       shm_table.shm_pages[i].refcnt = 1;
       mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+  p->sz = PGROUNDUP(p->sz)+PGSIZE;
 //       p->sz += PGSIZE;
       break;
     }
