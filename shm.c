5004d0aed416d281cc895f698a7d0e59a169db49
rruss003 <38058930+rruss003@users.noreply.github.com>
2019-12-02 09:27:13 -0800

    Update shm.c

--- shm.c
+++ shm.c
@@ -30,12 +30,14 @@ void shminit() {
 
 int shm_open(int id, char **pointer) {
 
+cprintf("SHM_OPEN\n");
 //you write this
 //Look through the shm_table to see if the id we are opening already exists
 int i = 0;
 int index = -1;
 initlock(&(shm_table.lock), "SHM lock");
 acquire(&(shm_table.lock));
+cprintf("before loop\n");
 for (i=0; i<64; i++){
   if(shm_table.shm_pages[i].id == id){
     index = i;
@@ -44,6 +46,7 @@ for (i=0; i<64; i++){
 }
 struct proc* p = myproc();
 if(index){
+  cprintf("CASE1\n");
   // Case 1
   mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[index].frame), PTE_W|PTE_U);
   shm_table.shm_pages[index].refcnt++;
@@ -52,6 +55,7 @@ if(index){
 else{
   // Case 2
   for(i = 0; i<64; i++){
+    cprintf("CASE2\n");
     if(shm_table.shm_pages[i].id == 0){
       shm_table.shm_pages[i].id = id;
       cprintf("BEFORE KALLOC\n");
@@ -71,6 +75,7 @@ return 0; //added to remove compiler warning -- you should decide what to return
 
 
 int shm_close(int id) {
+cprintf("SHM_CLOSE\n");
 //you write this too!
 
 
