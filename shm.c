064e2148ddcd0dbf197529203aac80ca5c8bf6cc
rruss003 <38058930+rruss003@users.noreply.github.com>
2019-12-02 10:53:44 -0800

    Update shm.c

--- shm.c
+++ shm.c
@@ -47,7 +47,7 @@ if(index > -1){
   mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[index].frame), PTE_W|PTE_U);
   shm_table.shm_pages[index].refcnt++;
   *pointer=(char *)PGROUNDUP(p->sz);
-  p->sz += PGSIZE;
+//   p->sz += PGSIZE;
 }
 else{
   // Case 2
@@ -56,10 +56,10 @@ else{
       shm_table.shm_pages[i].id = id;
       shm_table.shm_pages[i].frame = kalloc();
       shm_table.shm_pages[i].refcnt = 1;
-//       memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+      memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
       mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
       *pointer=(char *)PGROUNDUP(p->sz);
-      p->sz += PGSIZE;
+//       p->sz += PGSIZE;
       break;
     }
   }
