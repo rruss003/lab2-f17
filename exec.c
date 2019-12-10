4d241a0cb11feab1424851b8d20bfc92ca3f6a35
rruss003 <38058930+rruss003@users.noreply.github.com>
2019-12-09 17:14:19 -0800

    Update exec.c

--- exec.c
+++ exec.c
@@ -62,6 +62,7 @@ exec(char *path, char **argv)
 
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
+  cprintf("test Allocate two pages at the next page boundary\n");
   sz = PGROUNDUP(sz);
   if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
     goto bad;
@@ -101,6 +102,7 @@ exec(char *path, char **argv)
   curproc->tf->esp = sp;
   switchuvm(curproc);
   freevm(oldpgdir);
+  cprintf("test commit to the user image\n");
   return 0;
 
  bad:
