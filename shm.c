c6665766361566b2f74ab7c8d458a906bf4b1616
rruss003 <38058930+rruss003@users.noreply.github.com>
2019-12-09 16:43:16 -0800

    Update shm.c

--- shm.c
+++ shm.c
@@ -28,63 +28,139 @@ void shminit() {
   release(&(shm_table.lock));
 }
 
-int shm_open(int id, char **pointer) {
+// int shm_open(int id, char **pointer) {
 
-// you write this
-// Look through the shm_table to see if the id we are opening already exists
-int i = 0;
-int index = -1;
-acquire(&(shm_table.lock));
-for (i=0; i<64; i++){
-  if(shm_table.shm_pages[i].id == id){
-    index = i;
-    break;
-  }
-}
-struct proc* p = myproc();
-if(index > -1){
-  // Case 1
-  mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[index].frame), PTE_W|PTE_U);
-  shm_table.shm_pages[index].refcnt++;
-  *pointer=(char *)PGROUNDUP(p->sz);
-  p->sz += PGSIZE;
-}
-else{
-  // Case 2
-  for(i = 0; i<64; i++){
-    if(shm_table.shm_pages[i].id == 0){
-      shm_table.shm_pages[i].id = id;
-      shm_table.shm_pages[i].frame = kalloc();
-      shm_table.shm_pages[i].refcnt = 1;
-      memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
-      mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
-      *pointer=(char *)PGROUNDUP(p->sz);
-      p->sz += PGSIZE;
-      break;
-    }
-  }
-}
+// // you write this
+// // Look through the shm_table to see if the id we are opening already exists
+// int i = 0;
+// int index = -1;
+// acquire(&(shm_table.lock));
+// for (i=0; i<64; i++){
+//   if(shm_table.shm_pages[i].id == id){
+//     index = i;
+//     break;
+//   }
+// }
+// struct proc* p = myproc();
+// if(index > -1){
+//   // Case 1
+//   mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[index].frame), PTE_W|PTE_U);
+//   shm_table.shm_pages[index].refcnt++;
+//   *pointer=(char *)PGROUNDUP(p->sz);
+//   p->sz += PGSIZE;
+// }
+// else{
+//   // Case 2
+//   for(i = 0; i<64; i++){
+//     if(shm_table.shm_pages[i].id == 0){
+//       shm_table.shm_pages[i].id = id;
+//       shm_table.shm_pages[i].frame = kalloc();
+//       shm_table.shm_pages[i].refcnt = 1;
+//       memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+//       mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+//       *pointer=(char *)PGROUNDUP(p->sz);
+//       p->sz += PGSIZE;
+//       break;
+//     }
+//   }
+// }
   
-release(&(shm_table.lock));
-return 0; //added to remove compiler warning -- you should decide what to return
-}
+// release(&(shm_table.lock));
+// return 0; //added to remove compiler warning -- you should decide what to return
+// }
 
 
-int shm_close(int id) {
-int i=0;
-  initlock(&(shm_table.lock), "SHM lock");
-acquire(&(shm_table.lock));
-for (i=0; i<64; i++){
-  if(shm_table.shm_pages[i].id == id){
-    shm_table.shm_pages[i].refcnt--;
-    if(shm_table.shm_pages[i].refcnt == 0){
-      shm_table.shm_pages[i].id =0;
-      shm_table.shm_pages[i].frame =0;
-    }
-    break;
+// int shm_close(int id) {
+// int i=0;
+//   initlock(&(shm_table.lock), "SHM lock");
+// acquire(&(shm_table.lock));
+// for (i=0; i<64; i++){
+//   if(shm_table.shm_pages[i].id == id){
+//     shm_table.shm_pages[i].refcnt--;
+//     if(shm_table.shm_pages[i].refcnt == 0){
+//       shm_table.shm_pages[i].id =0;
+//       shm_table.shm_pages[i].frame =0;
+//     }
+//     break;
+//   }
+// }
+// release(&(shm_table.lock));
+
+// return 0; //added to remove compiler warning -- you should decide what to return
+// }
+int shm_open(int id, char **pointer) {
+  uint size = myproc()->sz;
+  int i;
+  if(id <= 0){//invalid ID
+    return -1;
   }
+  acquire(&(shm_table.lock));
+    for(i=0;i<64;i++){ //look for ID
+      if(id == shm_table.shm_pages[i].id){
+        if(shm_table.shm_pages[i].refcnt <= 0 || shm_table.shm_pages[i].frame == 0){
+          release(&(shm_table.lock));
+          return -1;
+        }
+        //ELSE MAP page
+        if(mappages(myproc()->pgdir,(char*)PGROUNDUP(size),PGSIZE,V2P(shm_table.shm_pages[i].frame),(PTE_W|PTE_U))==-1){
+          release(&(shm_table.lock));
+          return -1;
+        }
+        //ELSE done successsfully
+        shm_table.shm_pages[i].refcnt++;
+        *pointer=(char*)size;
+        release(&(shm_table.lock));
+        return 0;
+      }
+    }
+    //ID is not FOUND
+    for(i=0;i<64;i++){
+    if(shm_table.shm_pages[i].id == 0){ //Unused page found
+      shm_table.shm_pages[i].frame = kalloc();
+      memset(shm_table.shm_pages[i].frame,0,PGSIZE);
+      if(mappages(myproc()->pgdir,(char*)PGROUNDUP(size),PGSIZE,V2P(shm_table.shm_pages[i].frame),(PTE_W|PTE_U))==-1){
+        release(&(shm_table.lock));
+        return -1;
+      }
+      //ELSE done successsfully
+      shm_table.shm_pages[i].id = id;
+      if(shm_table.shm_pages[i].refcnt != 0){
+        release(&(shm_table.lock));
+        return -1;
+      }
+      shm_table.shm_pages[i].refcnt++;
+      *pointer=(char*)size;
+      release(&(shm_table.lock));
+      return 0;
+    }
+  }
+  release(&(shm_table.lock));//NO free pages
+  return -1; //added to remove compiler warning -- you should decide what to return
 }
-release(&(shm_table.lock));
 
-return 0; //added to remove compiler warning -- you should decide what to return
+// CS 153 - Lab 4: addition of all code
+int shm_close(int id) {
+  if(id <= 0){
+    return -1;
+  }
+  int i;
+  acquire(&(shm_table.lock));
+  for(i=0;i<64;i++){ //Find page with ID
+    if(id == shm_table.shm_pages[i].id){
+      if(shm_table.shm_pages[i].refcnt == 0){//Error Just In Case
+        release(&(shm_table.lock));
+        return -1;
+      }
+      shm_table.shm_pages[i].refcnt--;
+      if(shm_table.shm_pages[i].refcnt == 0){ //if page is not being used anymore
+        shm_table.shm_pages[i].id =0;
+        shm_table.shm_pages[i].frame =0;
+      }
+      release(&(shm_table.lock));
+      return 0;
+    }
+  }
+  //ID NOT FOUND
+  release(&(shm_table.lock));
+  return -1; //added to remove compiler warning -- you should decide what to return
 }
